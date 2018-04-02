4d10a5bf47255ba5d597972b74c882365d7ca6a7
zhaiyongyi <zhaiyongyi@henhaoji.com>
2018-04-02 20:44:49 +0800

    提交

--- HR/HRSystem/src/org/crazyit/hrsystem/action/LoginAction.java
+++ HR/HRSystem/src/org/crazyit/hrsystem/action/LoginAction.java
@@ -10,11 +10,11 @@ import java.util.List;
 import static org.crazyit.hrsystem.service.EmpManager.*;
 
 public class LoginAction extends EmpBaseAction {
-    //定义一个常量作为员工登录成功的Result名
+    //???????????????Result?
     private final String EMP_RESULT = "emp";
-    //定义一个常量作为经理登录成功的Result名
+    //???????????????Result?
     private final String MGR_RESULT = "mgr";
-    //封装请求参数
+    //??????
     private Manager manager;
 
     public Manager getManager() {
@@ -24,37 +24,37 @@ public class LoginAction extends EmpBaseAction {
         this.manager = manager;
     }
 
-    //struts2继承ActionSupport后执行action方法
+    //struts2??ActionSupport???action??
     @Override
     public String execute() throws Exception {
 
         System.out.println("---------------------------------");
         ActionContext ctx = ActionContext.getContext();
-        //获取HTTPSession中的rand属性
+        //??HTTPSession??rand??
 //        String ver2 = (String)ctx.getSession().get("rand");
 //        if(vercode.equalsIgnoreCase(ver2)){
-            //调用业务逻辑方法来处理登录请求
-        if(mgr == null) System.out.println("is null");
-        else System.out.println("not null");
+            //???????????????
+        if(mgr == null) System.out.println("is null ???????");
+        else System.out.println("not null ?????????");
         int result = mgr.valiadLogin(getManager());
-        //登录结果为普通员工
+        //?????????
         if(result == LOGIN_EMP){
             ctx.getSession().put(WebConstant.USER, manager.getName());
             ctx.getSession().put(WebConstant.LEVEL, WebConstant.EMP_LEVEL);
-            addActionMessage("您已成功登录系统");
+            addActionMessage("????????");
             return EMP_RESULT;
-        }else if (result == LOGIN_MGR){//登录结果为经理
+        }else if (result == LOGIN_MGR){//???????
             ctx.getSession().put(WebConstant.USER, manager.getName());
             ctx.getSession().put(WebConstant.LEVEL, WebConstant.MGR_LEVEL);
-            addActionMessage("您已成功登录系统");
+            addActionMessage("????????");
             return MGR_RESULT;
-        }else{//用户名和密码不匹配
-            addActionMessage("用户名/密码不匹配");
+        }else{//?????????
+            addActionMessage("???/?????");
             return ERROR;
         }
 //        }
 
-        //addActionMessage("验证码不匹配，请重新输入");
+        //addActionMessage("????????????");
        // return ERROR;
     }
 }
